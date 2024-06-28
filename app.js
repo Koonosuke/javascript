a86a56ea308b688387928b8ecfef1f83cbc9170d
koonosuke <149277895+Koonosuke@users.noreply.github.com>
2024-06-28 10:02:06 +0900

    syuusei

--- app.js
+++ app.js
@@ -1,51 +1,33 @@
-const express = require('express');
-const expressWs = require('express-ws');
-const { Pool } = require('pg');
-const { v4: uuidv4 } = require('uuid');
+const express = require('express')
+const expressWs = require('express-ws')
 
-const app = express();
-expressWs(app);
+const app = express()
+expressWs(app)
 
-const port = process.env.PORT || 3001;
-const connects = [];
+const port = process.env.PORT || 3001
+let connects = []
 
-// PostgreSQLの接続プールを作成
-const pool = new Pool({
-  host: 'localhost',
-  database: 'test',
-  user: 'kishi',
-  password: 'kishi1021',
-  port: 5432,
-});
-
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.ws('/ws', (ws, req) => {
-  connects.push(ws);
+  connects.push(ws)
 
-  ws.on('message', async (message) => {
-    console.log('Received:', message);
-
-    const [senderId, messageText] = message.split(':');
-    const timestamp = new Date();
-
-    // メッセージをPostgreSQLに保存
-    const query = 'INSERT INTO messages (sender_id, message_text, timestamp) VALUES ($1, $2, $3)';
-    await pool.query(query, [senderId, messageText, timestamp]);
+  ws.on('message', (message) => {
+    console.log('Received:', message)
 
     connects.forEach((socket) => {
       if (socket.readyState === 1) {
         // Check if the connection is open
-        socket.send(message);
+        socket.send(message)
       }
-    });
-  });
+    })
+  })
 
   ws.on('close', () => {
-    connects = connects.filter((conn) => conn !== ws);
-  });
-});
+    connects = connects.filter((conn) => conn !== ws)
+  })
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  console.log(`Server is running on http://localhost:${port}`)
+})
